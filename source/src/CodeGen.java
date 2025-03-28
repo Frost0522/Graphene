5c60d8bf0dba6224667ebfb97c3388d28d0e27aa
Frost0522 <Jmfrost522@gmail.com>
2025-03-27 21:35:23 -0500

    added functions for storing static data

--- source/src/CodeGen.java
+++ source/src/CodeGen.java
@@ -1,87 +1,58 @@
 package src;
-import java.util.ArrayList;
 import java.util.Stack;
-import src.SymbolTable.FunctionSymbol;
 
 public class CodeGen implements AstVisitor {
 
     private SymbolTable symbolTable;
     private Stack<StackFrame> callStack = new Stack<>();
     private StringBuilder imem = new StringBuilder();
-    private int fp, counter;
+    private int[] rmem = new int[8];
+    private int fp = 1, tos = 1;
 
-    private void makeFrame(String name) throws Analyzer {
-        callStack.push(new StackFrame(symbolTable.get(name)));
+    private void ldConst(int register, int constant) {
+        imem.append(rmem[7]+": LDC "+register+","+constant+"(0)\n");
+        rmem[register] = constant; rmem[7]++;
+    }
+    
+    private void stConst(int r1, int offset, int r2) {
+        imem.append(rmem[7]+": ST "+r1+","+offset+"("+r2+")\n");
+        rmem[7]++; rmem[5]++; tos++; rmem[6]++; fp++;
     }
 
-    protected String getImem() {return imem.toString();}
+    private void storeStaticData() {
+        int reg = 0;
+        for (int con : symbolTable.getStaticData().keySet()) {
+            if (reg<4) {ldConst(reg,con); reg++;}
+            else {ldConst(reg, con); stConst(reg,0,reg+1);}
+        }
+    }
     
     @Override
     public void visit(PrgrmNode prgrmNode) throws Analyzer {
-        symbolTable = prgrmNode.getSymbolTable(); fp = 1;
-        makeFrame("main");
-        for (String fnName : symbolTable.getFnNames()) {
-            if (!fnName.equals("main")) {makeFrame(fnName);}
-        }
-        for (StackFrame frame : callStack) {
-            imem.append(frame.code);
-        }
+        rmem[5] = tos; rmem[6] = fp;
+        symbolTable = prgrmNode.getSymbolTable();
+        storeStaticData();
     }
 
     class StackFrame implements AstVisitor {
 
-        private StringBuilder code = new StringBuilder();
-        private int currentInt, controlLink;
-        private String frameName;
-        private ArrayList<Integer> staticData;
+        private int[] rmem = new int[8];
+        private int size = 0;
 
-        public StackFrame(FunctionSymbol symbol) throws Analyzer {
-            staticData = symbol.getStaticData();
-            frameName = symbol.getFnNode().getName();
-            if (frameName.equals("main")) {
-                // Check symbol for parameters to allocate.
-                for (Node param : symbol.getParamNodes()) {/* to-do */}
-                if (symbol.getStaticData().size()<6) {
-                    Integer register = 0;
-                    for (Integer i : symbol.getStaticData()) {
-                        code.append(counter+": LDC "+register+","+i+"(0)\n"); counter++;
-                        register++;
-                    }
-                }
-                code.append(counter+": LDA 6,1(7)\n"); counter++;
-                code.append(counter+": LDA 7,5(1)\n"); counter++;
-                code.append(counter+": OUT 0,0,0\n"); counter++;
-                code.append(counter+": HALT 0,0,0\n"); counter++;
-                for (Node node : symbol.getFnNode().getBodyNodes()) {node.accept(this);}
-            }
-        }
+        public StackFrame() {
 
-        @Override
-        public void visit(CallNode callNode) throws Analyzer {
-            code.append(counter+": ST 6,"+fp+"(1)\n"); counter++;
-            controlLink = fp; fp++;
-            code.append(counter+": LDA 6,1(7)\n"); counter++;
-            code.append(counter+": LDA 7,"+(counter+2)+"(1)\n"); counter++;
-            code.append(counter+": LD 7,1("+controlLink+")\n"); counter++;
-            if (callNode.getName().equals("print")) {
-                Node argNode = callNode.getArgs().getFirst();
-                argNode.accept(this);
-                if (argNode.getSemanticType().equals(Lex.INTEGER)) {
-                    if (staticData.contains(currentInt)) {
-                        code.append(counter+": OUT "+staticData.indexOf(currentInt)+",0,0\n"); counter++;
-                    }
-                    else {
-                        code.append(counter+": LDC 4,"+currentInt+"(0)\n"); counter++;
-                        code.append(counter+": OUT 4,0,0\n"); counter++;    
-                    }
-                }
-            code.append(counter+": LDA 7,0(6)\n"); counter++;
-            }
         }
+        
+    }
 
-        @Override
-        public void visit(LitNode litNode) throws Analyzer {
-            currentInt = litNode.getValue();
+    // Debbugging purposes
+    private void printRmem() {
+        StringBuilder b = new StringBuilder();
+        for (int i=0;i<8;i++) {
+            if (i==0) {b.append("("+rmem[i]+",");}
+            else if (i==7) {b.append(rmem[i]+")\n");}
+            else {b.append(rmem[i]+",");}
         }
+        System.out.println(b.toString());
     }
 }
