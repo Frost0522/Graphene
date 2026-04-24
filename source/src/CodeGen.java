1eecacf7589dec9b1cf79e015fbd902624af694b
Frost0522 <Jmfrost522@gmail.com>
2026-04-23 20:26:14 -0500

    Removed condition handler for IR generation of if nodes, added 'and', 'or', 'lessthan', and 'equivalent' code and IR generation to binary nodes, added code and IR generation for 'expression' and 'not' nodes.

--- source/src/CodeGen.java
+++ source/src/CodeGen.java
@@ -11,13 +11,16 @@ public class CodeGen implements AstVisitor {
     private CodeGen self = this;
     private SymbolTable symbolTable;
     private HashMap<String,StackFrame> stackFrameMap;
-    private ArrayList<Integer> rtrnLocs = new ArrayList<>();
     private ArrayList<Tac> triplesArray = new ArrayList<>();
-    private StringBuilder targetCode = new StringBuilder();
+    private StringBuilder targetCodeBuilder = new StringBuilder();
+    private ArrayList<String> targetCodeArray = new ArrayList<>();
     private Stack<Integer> labelStack = new Stack<>();
     private StackFrame frame = new StackFrame();
 
-    public String getTargetCode() {return targetCode.toString();}
+    public String getTargetCode() {
+        for (String s : targetCodeArray) {targetCodeBuilder.append(s);}
+        return targetCodeBuilder.toString();
+    }
 
     @Override
     public void visit(PrgrmNode prgrmNode) throws Analyzer {
@@ -31,8 +34,10 @@ public class CodeGen implements AstVisitor {
         symbolTable.getFunction("main").accept(this);
         write(Lex.EPILOGUE,"");
 
-        for (int i=0;i<triplesArray.size();i++) {System.out.println(i+" "+triplesArray.get(i));}
-        System.out.println(); genTargetCode(); System.out.println(targetCode); System.out.println();
+        genTargetCode();
+
+        // for (int i=0;i<triplesArray.size();i++) {System.out.println(i+" "+triplesArray.get(i));}
+        // System.out.println(); genTargetCode(); System.out.println(getTargetCode()); System.out.println();
     }
 
     @Override
@@ -85,7 +90,8 @@ public class CodeGen implements AstVisitor {
 
     @Override @SuppressWarnings("incomplete-switch")
     public void visit(IfNode ifNode) throws Analyzer {
-        ifNode.getIf().accept(new CondHandler());
+        ifNode.getIf().accept(this);
+        write(Lex.IF,ifNode.getIf().nodeType(),0);
         ifNode.getThen().accept(this);
         switch (ifNode.getThen().nodeType()) {
             case PLUS,MINUS,DIVIDE,TIMES,EQUIVALENT,LESSTHAN,AND,OR,ID,LITERAL: {
@@ -106,26 +112,29 @@ public class CodeGen implements AstVisitor {
 
         Lex rType = binNode.getRight().nodeType();
         Boolean rIsTerminal = (rType==Lex.ID || rType==Lex.LITERAL);
+        binNode.getLeft().accept(this);
 
         if (!rIsTerminal) {
-            binNode.getLeft().accept(this);
-            write(Lex.STORE,0,makeTemp(frame));
-            new Assign(binNode.getRight(),1);
-            write(Lex.LOAD,0,removeTemp(frame));
-        } else {
-            binNode.getLeft().accept(this);
-            new Assign(binNode.getRight(),1);
-        }
+            write(Lex.STORE,0,frame.size()); makeTemp(frame);
+            binNode.getRight().accept(this);
+            write(Lex.LOAD,1,frame.size()-1); removeTemp(frame);
+        } else {new Assign(binNode.getRight(),1);}
 
         switch (binNode.nodeType()) {
-            case PLUS,MINUS,TIMES,DIVIDE: {write(binNode.nodeType(),0,1); break;}
+            case PLUS,MINUS,TIMES,DIVIDE,AND,OR,LESSTHAN,EQUIVALENT: {write(binNode.nodeType(),0); break;}
         }
     }
 
     @Override
-    public void visit(LitNode litNode) {
-        write(Lex.CONST,litNode.getValue(),0);
+    public void visit(ExpNode expNode) throws Analyzer {expNode.getNode().accept(this);}
+
+    @Override
+    public void visit(NotNode notNode) throws Analyzer {
+        notNode.getNode().accept(this); write(notNode.nodeType(),"0");
     }
+
+    @Override
+    public void visit(LitNode litNode) {write(Lex.CONST,litNode.getValue(),0);}
 
     @Override
     public void visit(IdNode idNode) {
@@ -149,7 +158,6 @@ public class CodeGen implements AstVisitor {
             } write(Lex.MALLOC,newFrame.size());
             write(Lex.GOTO,newFrame.getName());
         }
-
     }
 
     class Assign implements AstVisitor {
@@ -163,46 +171,12 @@ public class CodeGen implements AstVisitor {
         }
 
         @Override
-        public void visit(LitNode litNode) {
-            write(Lex.CONST,litNode.getValue(),loc);
-        }
+        public void visit(LitNode litNode) {write(Lex.CONST,litNode.getValue(),loc);}
 
         @Override
         public void visit(IdNode idNode) {
             int i = stackFrameMap.get(currentFn.getName()).getParamIndex(idNode.getName());
             write(Lex.LOAD,loc,i);
-        }
-    }
-
-    class CondHandler implements AstVisitor {
-
-        @Override @SuppressWarnings("incomplete-switch")
-        public void visit(BinaryNode binNode) throws Analyzer {
-
-            // Special cases
-            if (binNode.nodeType()==Lex.EQUIVALENT) {
-                if (binNode.getLeft().getName().equals("0")) {
-                    binNode.getRight().accept(self); write(Lex.IF,Lex.EQUIVALENT,0);
-                } else if (binNode.getRight().getName().equals("0")) {
-                    binNode.getLeft().accept(self); write(Lex.IF,Lex.EQUIVALENT,0);
-                }
-            } else {
-                Lex rType = binNode.getRight().nodeType();
-                if (rType!=Lex.ID&&rType!=Lex.LITERAL) {
-                    binNode.getLeft().accept(self);
-                    write(Lex.STORE,0,makeTemp(frame));
-                    new Assign(binNode.getRight(),1);
-                    write(Lex.LOAD,0,removeTemp(frame));
-                } else {
-                    binNode.getLeft().accept(self);
-                    new Assign(binNode.getRight(),1);
-                }
-
-                switch (binNode.nodeType()) {
-                    case EQUIVALENT: {write(Lex.MINUS,0,1); write(Lex.IF,Lex.EQUIVALENT,0); break;}
-                    case LESSTHAN: {write(Lex.MINUS,0,1); write(Lex.IF,Lex.LESSTHAN,0); break;}
-                }
-            }
         }
     }
 
@@ -212,16 +186,17 @@ public class CodeGen implements AstVisitor {
         prevFrame = new StackFrame().clone(frame);
         for (Tac tac : triplesArray) {
             switch (tac.op) {
-                case ENTRY: {targetCode.append("* "+tac.arg1+"\n"); frame.setIns(insNum); break;}
-                case BEGINPROLOGUE: {targetCode.append("* prologue\n"); break;}
-                case ENDPROLOGUE: {targetCode.append("\n"); break;}
+                case ENTRY: {targetCodeArray.add("* "+tac.arg1+"\n"); frame.setIns(insNum); break;}
+                case BEGINPROLOGUE: {targetCodeArray.add("* prologue\n"); break;}
+                case ENDPROLOGUE: {targetCodeArray.add("\n"); break;}
                 case EPILOGUE: {
-                    for (int loc : rtrnLocs) {targetCode.replace(loc,loc+1,insNum+"");}
-                    targetCode.append(
-                        "\n* epilogue\n"+
-                        insNum+": OUT 0,0,0\n"+
-                        (insNum+1)+": HALT 0,0,0"
-                    ); insNum+=2; break;
+                    for (int i=0;i<targetCodeArray.size();i++) {
+                        targetCodeArray.set(i,targetCodeArray.get(i).replace("$",insNum+""));
+                    }
+                    targetCodeArray.add("\n* epilogue\n");
+                    targetCodeArray.add(insNum+": OUT 0,0,0\n");
+                    targetCodeArray.add((insNum+1)+": HALT 0,0,0");
+                    insNum+=2; break;
                 }
                 case CALL: {
                     if (!(tac.arg1.equals("print"))) {
@@ -231,76 +206,87 @@ public class CodeGen implements AstVisitor {
                     } break;
                 }
                 case BEGINCALL: {
-                    /* Advance the fp and tos */ targetCode.append(
-                        insNum+": LDA 5,0(6)\n"+(insNum+1)+": LDA 6,"+frame.size()+"(6)\n"+tac.arg1
-                    ); insNum+=2; break;
+                    /* Advance the fp and tos */ 
+                    targetCodeArray.add(insNum+": LDA 5,0(6)\n");
+                    targetCodeArray.add((insNum+1)+": LDA 6,"+frame.size()+"(6)\n"+tac.arg1);
+                    insNum+=2; break;
                 }
-                case CONST: {targetCode.append(insNum+": LDC "+tac.arg2+","+tac.arg1+"(0)\n"); insNum++; break;}
+                case CONST: {targetCodeArray.add(insNum+": LDC "+tac.arg2+","+tac.arg1+"(0)\n"); insNum++; break;}
                 case IF: {
-                    if (tac.arg1==Lex.EQUIVALENT) {targetCode.append(insNum+": JNE "+tac.arg2+",*");}
-                    else if (tac.arg1==Lex.LESSTHAN) {targetCode.append(insNum+": JGE "+tac.arg2+",*");}
-                    labelStack.push(targetCode.length()); targetCode.append("(4)\n"); insNum++; break;
+                    targetCodeArray.add(insNum+": JEQ "+tac.arg2+",$(4)\n");
+                    labelStack.push(targetCodeArray.size()-1); insNum++; break;
                 }
                 case ELSE: {
                     if (!labelStack.isEmpty()) {
-                        int label = labelStack.pop(); targetCode.replace(label-1,label,insNum+"");
+                        int label = labelStack.pop();
+                        targetCodeArray.set(label, targetCodeArray.get(label).replace("$",insNum+""));
                     } break;
                 }
-                case EQUIVALENT: {targetCode.append(insNum+": JNE "+tac.arg1+",*(4)\n"); insNum++; break;}
-                case PLUS: {targetCode.append(insNum+": ADD 0,"+tac.arg1+","+tac.arg2+"\n"); insNum++; break;}
-                case MINUS: {targetCode.append(insNum+": SUB 0,"+tac.arg1+","+tac.arg2+"\n"); insNum++; break;}
-                case TIMES: {targetCode.append(insNum+": MUL 0,"+tac.arg1+","+tac.arg2+"\n"); insNum++; break;}
-                case DIVIDE: {targetCode.append(insNum+": DIV 0,"+tac.arg1+","+tac.arg2+"\n"); insNum++; break;}
-                case EXIT: {break;}
-                case LOAD: {targetCode.append(insNum+": LD "+tac.arg1+","+tac.arg2+"(5)\n"); insNum++; break;}
-                case STORE: {targetCode.append(insNum+": ST "+tac.arg1+","+tac.arg2+"(5)\n"); insNum++; break;}
-                case PRINT: {targetCode.append(insNum+": OUT 0,0,0\n"); insNum++; break;}
+                case NOT: {
+                    targetCodeArray.add(insNum+": JEQ 0,2(7)\n");
+                    targetCodeArray.add((insNum+1)+": LDC 0,0(0)\n");
+                    targetCodeArray.add((insNum+2)+": LDA 7,1(7)\n");
+                    targetCodeArray.add((insNum+3)+": LDC 0,1(0)\n");
+                    insNum+=4; break;
+                }
+                case EQUIVALENT: {
+                    targetCodeArray.add(insNum+": SUB 0,0,1\n");
+                    targetCodeArray.add((insNum+1)+": JEQ 0,2(7)\n");
+                    targetCodeArray.add((insNum+2)+": LDC 0,0(0)\n");
+                    targetCodeArray.add((insNum+3)+": LDA 7,1(7)\n");
+                    targetCodeArray.add((insNum+4)+": LDC 0,1(0)\n");
+                    insNum+=5; break;
+                }
+                case LESSTHAN: {
+                    targetCodeArray.add(insNum+": SUB 0,0,1\n");
+                    targetCodeArray.add((insNum+1)+": JLT 0,2(7)\n");
+                    targetCodeArray.add((insNum+2)+": LDC 0,0(0)\n");
+                    targetCodeArray.add((insNum+3)+": LDA 7,1(7)\n");
+                    targetCodeArray.add((insNum+4)+": LDC 0,1(0)\n");
+                    insNum+=5; break;
+                }
+                case OR: {
+                    targetCodeArray.add(insNum+": ADD "+tac.arg1+",0,1\n");
+                    targetCodeArray.add((insNum+1)+": JEQ 0,1(7)\n");
+                    targetCodeArray.add((insNum+2)+": LDC 0,1(0)\n");
+                    insNum+=3; break;
+                }
+                case PLUS: {targetCodeArray.add(insNum+": ADD "+tac.arg1+",0,1\n"); insNum++; break;}
+                case MINUS: {targetCodeArray.add(insNum+": SUB "+tac.arg1+",0,1\n"); insNum++; break;}
+                case TIMES,AND: {targetCodeArray.add(insNum+": MUL "+tac.arg1+",0,1\n"); insNum++; break;}
+                case DIVIDE: {targetCodeArray.add(insNum+": DIV "+tac.arg1+",0,1\n"); insNum++; break;}
+                case LOAD: {targetCodeArray.add(insNum+": LD "+tac.arg1+","+tac.arg2+"(5)\n"); insNum++; break;}
+                case STORE: {targetCodeArray.add(insNum+": ST "+tac.arg1+","+tac.arg2+"(5)\n"); insNum++; break;}
+                case PRINT: {targetCodeArray.add(insNum+": OUT 0,0,0\n"); insNum++; break;}
                 case GOTO: {
-                    targetCode.append(insNum+": LDA 7,"+frame.getIns()+"(4)\n"); 
+                    targetCodeArray.add(insNum+": LDA 7,"+frame.getIns()+"(4)\n"); 
                     insNum++; StackFrame tmpFrame = new StackFrame().clone(frame);
                     frame.clone(prevFrame); prevFrame.clone(tmpFrame); break;
                 }
-                case ALTER: {
-                    if (tac.arg1.toString().startsWith("-")) {frame.addTmp();}
-                    else {frame.removeTmp();}
-                    targetCode.append(insNum+": LDA "+tac.arg2+","+tac.arg1+"("+tac.arg2+")\n"); insNum++;
+                case STEP: {
+                    if (tac.arg1.toString().startsWith("-")) {frame.removeTmp();}
+                    else {frame.addTmp();}
+                    targetCodeArray.add(insNum+": LDA "+tac.arg2+","+tac.arg1+"("+tac.arg2+")\n"); insNum++;
                     break;
                 }
                 case RESTORE: {
-                    targetCode.append(
-                        insNum+": LDA 6,0(5)\n"+
-                        (insNum+1)+": LDA 5,-"+tac.arg1+"(5)\n"
-                    ); insNum+=2; break;
+                    targetCodeArray.add(insNum+": LDA 6,0(5)\n");
+                    targetCodeArray.add((insNum+1)+": LDA 5,-"+tac.arg1+"(5)\n");
+                    insNum+=2; break;
                 }
                 case MALLOC: {
-                    targetCode.append(
-                        insNum+": LDA 5,0(6)\n"+
-                        (insNum+1)+": LDA 6,"+tac.arg1+"(6)\n"+
-                        (insNum+2)+": LDA 0,2(7)\n"+
-                        (insNum+3)+": ST 0,2(5)\n"
-                    ); insNum+=4; break;
-                }
-                case COPY: {
-                    targetCode.append(
-                        insNum+": LD 0,"+tac.arg1+"(5)\n"+
-                        (insNum+1)+": ST 0,"+tac.arg2+"(5)\n"
-                    ); insNum+=2; break;
-                }
-                case SWAP: {
-                    targetCode.append(
-                        insNum+": LD 0,"+tac.arg1+"(5)\n"+
-                        (insNum+1)+": LD 1,"+tac.arg2+"(5)\n"+
-                        (insNum+2)+": ST 0,"+tac.arg2+"(5)\n"+
-                        (insNum+3)+": ST 1,"+tac.arg1+"(5)\n"
-                    ); insNum+=4; break;
+                    targetCodeArray.add(insNum+": LDA 5,0(6)\n");
+                    targetCodeArray.add((insNum+1)+": LDA 6,"+tac.arg1+"(6)\n");
+                    targetCodeArray.add((insNum+2)+": LDA 0,2(7)\n");
+                    targetCodeArray.add((insNum+3)+": ST 0,2(5)\n");
+                    insNum+=4; break;
                 }
                 case RETURN: {
-                    targetCode.append(
-                        insNum+": LD 1,"+frame.getStateLoc()+"(5)\n"+
-                        (insNum+1)+": JNE 1,"+(insNum+3)+"(4)\n"+
-                        (insNum+2)+": LDA 7,*(4)\n"
-                    ); insNum+=3; rtrnLocs.add(targetCode.length()-4);
-                    targetCode.append(insNum+": LDA 7,0(1)\n"); insNum++; break;
+                    targetCodeArray.add(insNum+": LD 1,"+frame.getStateLoc()+"(5)\n");
+                    targetCodeArray.add((insNum+1)+": JNE 1,"+(insNum+3)+"(4)\n");
+                    targetCodeArray.add((insNum+2)+": LDA 7,$(4)\n");
+                    targetCodeArray.add((insNum+3)+": LDA 7,0(1)\n");
+                    insNum+=4; break;
                 }
             }
         }
@@ -320,6 +306,6 @@ public class CodeGen implements AstVisitor {
 
     private void write(Lex op, Object arg1, Object arg2) {triplesArray.add(new Tac(op, arg1, arg2));}
     private void write(Lex op, Object arg1) {triplesArray.add(new Tac(op, arg1));}
-    private int makeTemp(StackFrame f) {write(Lex.ALTER,1,6); return f.addTmp();}
-    private int removeTemp(StackFrame f) {write(Lex.ALTER,-1,6); return f.removeTmp();}
+    private void makeTemp(StackFrame f) {f.addTmp(); write(Lex.STEP,1,6);}
+    private void removeTemp(StackFrame f) {f.removeTmp(); write(Lex.STEP,-1,6);}
 }
