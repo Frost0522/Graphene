6c8a3d5d2f2d892d4df01b9f498c5ccf77c5e5f3
Frost0522 <Jmfrost522@gmail.com>
2026-04-23 20:08:32 -0500

    Refactored, and added missmatching operand check for equivalency operations.

--- source/src/Analyzer.java
+++ source/src/Analyzer.java
@@ -4,17 +4,14 @@ import java.util.Stack;
 
 public class Analyzer extends Throwable {
 
-    public Analyzer(String msg) {
-        System.err.println(msg); System.exit(0);
-    }
+    public Analyzer(String msg) {System.err.println(msg); System.exit(0);}
 
     private void _defaultMsg(int line, int column, String topOfStack, Token errorToken, StringBuilder errorName) throws Analyzer {
         if (errorToken.getType().equals(Lex.ID) || errorToken.getType().equals(Lex.FNCALL) || 
             errorToken.getType().equals(Lex.INTEGERLITERAL) || errorToken.getType().equals(Lex.BOOLEANLITERAL)) {
             throw new Analyzer("Line "+line+" Column "+column+"\nParsing Error: Stack rule "+"'"+topOfStack+"'"+
                                " encountered unexpected "+errorToken.getType().toString().toLowerCase()+" token "+"'"+errorName+"'.\n");
-        }
-        throw new Analyzer("Line "+errorToken.line+" Column "+errorToken.column+"\nParsing Error: Stack rule "+"'"+topOfStack+"'"+
+        } throw new Analyzer("Line "+errorToken.line+" Column "+errorToken.column+"\nParsing Error: Stack rule "+"'"+topOfStack+"'"+
                            " encountered unexpected token '"+errorName+"'.\n");
     }
 
@@ -39,154 +36,122 @@ public class Analyzer extends Throwable {
                 if (listContains(errorTokenType,Lex.definitionListError())) {
                     _defaultMsg(errorToken.line, errorToken.column, topOfStack, errorToken, errorName);
                 } break;
-
             case DEFINITION:
                 if (listContains(errorTokenType,Lex.definitionError())) {
                     _defaultMsg(errorToken.line, errorToken.column, topOfStack, errorToken, errorName);
                 } break;
-
             case PARAMLIST:
                 if (errorToken.getType().equals(Lex.RETURN)) {
                     errorToken = deadLst.get(deadLst.size()-1);
                     throw new Analyzer("Line "+errorToken.line+" Column "+(errorToken.column+errorToken.getSize())+"\nParsing Error: Missing right parenthesis.\n");
-                }
-                else if (listContains(errorTokenType,Lex.parameterListError())) {
+                } else if (listContains(errorTokenType,Lex.parameterListError())) {
                     _defaultMsg(errorToken.line, errorToken.column, topOfStack, errorToken, errorName);
                 } break;
-
             case ARGLIST:
                 if (listContains(errorTokenType,Lex.arugmentListError())) {
                     _defaultMsg(errorToken.line, errorToken.column, topOfStack, errorToken, errorName);
                 } break;
-
             case BODY:
                 if (errorToken.getType().equals(Lex.$)) {
                     throw new Analyzer("Line "+errorToken.line+" Column "+errorToken.column+"\nParsing Error: Function body not found.\n");
-                }
-                else if (listContains(errorTokenType,Lex.isOperator())) {
+                } else if (listContains(errorTokenType,Lex.isOperator())) {
                     throw new Analyzer("Line "+errorToken.line+" Column "+errorToken.column+"\nParsing Error: Stack rule "+"'"+topOfStack+"'"+
                                        " encountered unexpected operator '"+errorName+"'.\n");
-                }
-                else if (listContains(errorTokenType,Lex.bodyError())) {
+                } else if (listContains(errorTokenType,Lex.bodyError())) {
                     _defaultMsg(errorToken.line, errorToken.column, topOfStack, errorToken, errorName);
                 } break;
-
             case EXP:
                 if (listContains(errorTokenType,Lex.isOperator())) {
                     throw new Analyzer("Line "+errorToken.line+" Column "+errorToken.column+"\nParsing Error: Stack rule "+"'"+topOfStack+"'"+
                                        " encountered unexpected operator '"+errorName+"'.\n");
-                } 
-                else if (listContains(errorTokenType,Lex.expressionError())) {
+                } else if (listContains(errorTokenType,Lex.expressionError())) {
                     _defaultMsg(errorToken.line, errorToken.column, topOfStack, errorToken, errorName);
                 } break;
-
             case EXPTAIL:
                 if (listContains(errorTokenType,Lex.expressionTailError())) {
                     _defaultMsg(errorToken.line, errorToken.column, topOfStack, errorToken, errorName);
                 } break;
-
             case FACTOR:
                 if (listContains(errorTokenType,Lex.factorError())) {
                     _defaultMsg(errorToken.line, errorToken.column, topOfStack, errorToken, errorName);
                 } break;
-
             case FORMALARGTAIL:
                 if (listContains(errorTokenType,Lex.formalArgsTailError())) {
                     _defaultMsg(errorToken.line, errorToken.column, topOfStack, errorToken, errorName);
                 } break;
-
             case FORMALARG:
                 if (listContains(errorTokenType,Lex.formalArgsError())) {
                     _defaultMsg(errorToken.line, errorToken.column, topOfStack, errorToken, errorName);
                 } break;
-
             case FORMALPARAM:
                 if (listContains(errorTokenType,Lex.formalParamError())) {
                     _defaultMsg(errorToken.line, errorToken.column, topOfStack, errorToken, errorName);
                 } break;
-
             case FORMALPARAMTAIL:
                 if (listContains(errorTokenType,Lex.formalParamTailError())) {
                     _defaultMsg(errorToken.line, errorToken.column, topOfStack, errorToken, errorName);
-                }
-                break;
-
+                } break;
             case IDWITHTYPE:
                 if (listContains(errorTokenType,Lex.idWithTypeError())) {
                     _defaultMsg(errorToken.line, errorToken.column, topOfStack, errorToken, errorName);
                 } break;
-
             case LITERAL:
                 if (listContains(errorTokenType,Lex.literalError())) {
                     _defaultMsg(errorToken.line, errorToken.column, topOfStack, errorToken, errorName);
                 } break;
-
             case PRINTEXP:
                 if (listContains(errorTokenType,Lex.printExpError())) {
                     _defaultMsg(errorToken.line, errorToken.column, topOfStack, errorToken, errorName);
                 } break;
-
             case PROGRAM:
                 if (listContains(errorTokenType,Lex.programError())) {
                     _defaultMsg(errorToken.line, errorToken.column, topOfStack, errorToken, errorName);
                 } break;
-
             case SIMPLEEXP:
                 if (listContains(errorTokenType,Lex.simpleExpError())) {
                     if (errorToken.getType().equals(Lex.$)) {
                         throw new Analyzer("Line "+errorToken.line+" Column "+errorToken.column+"\nParsing Error: Stack rule "+"'"+topOfStack+"'"+
                                            " encountered unexpected end of file.\n");
-                    }
-                    _defaultMsg(errorToken.line, errorToken.column, topOfStack, errorToken, errorName);
+                    } _defaultMsg(errorToken.line, errorToken.column, topOfStack, errorToken, errorName);
                 } break;
-
             case SIMPLEEXPTAIL:
                 if (listContains(errorTokenType,Lex.simpleExpTailError())) {
                     _defaultMsg(errorToken.line, errorToken.column, topOfStack, errorToken, errorName);
                 } break;
-
             case TERM:
                 if (listContains(errorTokenType,Lex.termError())) {
                     if (errorToken.getType().equals(Lex.$)) {
                         throw new Analyzer("Line "+errorToken.line+" Column "+errorToken.column+"\nParsing Error: Stack rule "+"'"+topOfStack+"'"+
                                            " encountered unexpected end of file.\n");
-                    }
-                    _defaultMsg(errorToken.line, errorToken.column, topOfStack, errorToken, errorName);
+                    } _defaultMsg(errorToken.line, errorToken.column, topOfStack, errorToken, errorName);
                 } break;
-
             case TERMTAIL:
                 if (listContains(errorTokenType,Lex.termTailError())) {
                     _defaultMsg(errorToken.line, errorToken.column, topOfStack, errorToken, errorName);
                 } break;
-
             case TYPE:
                 if (listContains(errorTokenType,Lex.typeError())) {
                     errorToken = deadLst.get(deadLst.size()-1);
                     throw new Analyzer("Line "+errorToken.line+" Column "+(errorToken.column+errorToken.getSize())+
                                        "\nParsing Error: Missing function return type.\n");
                 } break;
-
             case ID: 
                 if (errorToken.getType().equals(Lex.LEFTPAREN)) {
                     throw new Analyzer("Line "+errorToken.line+" Column "+errorToken.column+"\nParsing Error: Missing function name.\n");
                 } break;
-
             case RETURN:
                 if (errorToken.getType().equals(Lex.INTEGER) || errorToken.getType().equals(Lex.BOOLEAN)) {
                     throw new Analyzer("Line "+errorToken.line+" Column "+errorToken.column+"\nParsing Error: Missing return symbol.\n");                    
                 } break;
-
             case LEFTPAREN:
                 if (errorToken.getType().equals(Lex.RIGHTPAREN)) {
                     throw new Analyzer("Line "+errorToken.line+" Column "+errorToken.column+"\nParsing Error: Missing left parenthesis.\n");
-                }
-                else if (listContains(errorTokenType,Lex.isOperator())) {
+                } else if (listContains(errorTokenType,Lex.isOperator())) {
                     StringBuilder newErrorName = new StringBuilder(); 
                     for (int i : deadLst.get(deadLst.size()-1).getCharList()) {newErrorName.append((char) i);}
                     throw new Analyzer("Line "+errorToken.line+" Column "+errorToken.column+"\nParsing Error: Keyword '"+newErrorName+
                                        "' encountered unexpected token '"+errorTokenTypeName+"'.\n");
                 } break;
-
             case MINUS:
                 StringBuilder lastTokenName = new StringBuilder();
                 for (int i : deadLst.get(deadLst.size()-1).getCharList()) {lastTokenName.append((char) i);}
@@ -198,10 +163,7 @@ public class Analyzer extends Throwable {
         if (errorToken.getType().equals(Lex.$)) {
             throw new Analyzer("Line "+errorToken.line+" Column "+errorToken.column+"\nParsing Error: Stack rule "+"'"+topOfStack+"'"+
                                " encountered unexpected end of file.\n");
-        }
-        else {
-            _defaultMsg(errorToken.line, errorToken.column, topOfStack, errorToken, errorName);
-        }
+        } else {_defaultMsg(errorToken.line, errorToken.column, topOfStack, errorToken, errorName);}
     }
 
     // Error handling for the semantic analyzer.
@@ -243,8 +205,7 @@ public class Analyzer extends Throwable {
                 if (node.nodeType()==Lex.ID && node.getSemanticType()==null) {
                     throw new Analyzer("Line "+line+" Column "+column+"\nSemantic Error: Unassigned "+node.getErrorStr()+", check that the"+
                                        " parameter has been declared.");
-                }
-                throw new Analyzer("Line "+line+" Column "+column+"\nSemantic Error: Type mismatch between function return type and function body.");
+                } throw new Analyzer("Line "+line+" Column "+column+"\nSemantic Error: Type mismatch between function return type and function body.");
             }
             case NOFNCALL: {
                 throw new Analyzer("Line "+line+" Column "+column+"\nSemantic Error: Unassigned "+node.getErrorStr()+", check that the function "+
@@ -268,6 +229,9 @@ public class Analyzer extends Throwable {
                 throw new Analyzer("Line "+line+" Column "+column+"\nSemantic Error: Function call does not take postional argument "+
                                    node.getSemanticType().toString().toLowerCase()+".");
             }
+            case DIFFOPERANDS: {
+                throw new Analyzer("Line "+line+" Column "+column+"\nSemantic Error: Missmatch operand types.");
+            }
             case IFOPERROR: {
                 throw new Analyzer("Line "+line+" Column "+column+"\nSemantic Error: If conditions must be of type boolean.");
             }
@@ -288,17 +252,13 @@ public class Analyzer extends Throwable {
             case SIGNANDTYPEMISSMATCH: {
                 if (node.getName().equals("print")) {
                     throw new Analyzer("Line "+line+" Column "+column+"\nSemantic Error: Primitive 'print' must not be negative.");
-                }
-                if (node.nodeType()==Lex.FNCALL) {
+                } if (node.nodeType()==Lex.FNCALL) {
                     throw new Analyzer("Line "+line+" Column "+column+"\nSemantic Error: Function calls of type boolean must not be negative.");
-                }
-                if (node.nodeType()==Lex.EXP) {
+                } if (node.nodeType()==Lex.EXP) {
                     throw new Analyzer("Line "+line+" Column "+column+"\nSemantic Error: Expressions of type boolean must not be negative.");
-                }
-                if (node.nodeType()==Lex.ID) {
+                } if (node.nodeType()==Lex.ID) {
                     throw new Analyzer("Line "+line+" Column "+column+"\nSemantic Error: Identifiers of type boolean must not be negative.");
-                }
-                if (node.nodeType()==Lex.LITERAL) {
+                } if (node.nodeType()==Lex.LITERAL) {
                     throw new Analyzer("Line "+line+" Column "+column+"\nSemantic Error: Boolean literals must not be negative.");
                 }
             }
@@ -307,10 +267,8 @@ public class Analyzer extends Throwable {
         // Warning messages
         if (errorType==Lex.UNUSEDFN) {
             System.err.println("Warning @"+line+":"+column+" -> Function '"+node.getName()+"' is never used.");
-        }
-        else if (errorType==Lex.UNUSEDPARAM) {
+        } else if (errorType==Lex.UNUSEDPARAM) {
             System.err.println("Warning @"+line+":"+column+" -> Parameter '"+node.toString().replace("identifier ","")+"' is never used.");
-        }
-        else {throw new Analyzer("Unexpected Semantic Error.");}
+        } else {throw new Analyzer("Unexpected Semantic Error.");}
     }
 }
